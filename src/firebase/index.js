9b93a107562bfc440bf4e59273916ee2a3472508
TamTran72111 <tam.tran72111@gmail.com>
2020-11-12 15:50:58 -0500

    Add firebase and redux

---
+++ src/firebase/index.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/storage";
+import "firebase/auth";
+
+import firebaseConfig from "./config";
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const db = firebaseApp.firestore();
+export const auth = firebaseApp.auth();
+export const firestore = firebase.firestore;
+export const storage = firebaseApp.storage();
