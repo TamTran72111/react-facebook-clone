23d65c3143fc1e855f05b7b6a05eb28bf21b2ead
TamTran72111 <tam.tran72111@gmail.com>
2020-11-23 12:23:31 -0500

    Add delay when fetching comments to avoid fetching deleted ones in firebase cache

--- src/redux/actions/posts.js
+++ src/redux/actions/posts.js
@@ -1,11 +1,11 @@
-import { db } from "../../firebase";
-import { fetchPostComments } from "./comments";
-import { CLEANUP_POSTS, FETCH_POSTS, LISTEN_POSTS } from "./types";
+import { db } from '../../firebase';
+import { fetchPostComments } from './comments';
+import { CLEANUP_POSTS, FETCH_POSTS, LISTEN_POSTS } from './types';
 
-const Posts = db.collection("posts");
+const Posts = db.collection('posts');
 
 export const fetchAllPosts = () => (dispatch) => {
-  const unsubscribe = Posts.orderBy("created_at", "desc").onSnapshot(
+  const unsubscribe = Posts.orderBy('created_at', 'desc').onSnapshot(
     (snapshot) => {
       // Update posts on snapshot change
       const posts = snapshot.docs.map((post) => ({
@@ -16,8 +16,8 @@ export const fetchAllPosts = () => (dispatch) => {
 
       // Fetch comments for changed posts (added and modified only)
       snapshot.docChanges().forEach((change) => {
-        if (change.type === "modified" || change.type === "added") {
-          fetchPostComments(dispatch, change.doc.id);
+        if (change.type === 'modified' || change.type === 'added') {
+          setTimeout(() => fetchPostComments(dispatch, change.doc.id), 100);
         }
       });
     }
